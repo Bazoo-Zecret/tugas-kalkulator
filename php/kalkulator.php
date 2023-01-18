491b148cd7add3b52aeaf34efb0b23fbe61ae7cf
Bazoo-Zecret <114378418+Bazoo-Zecret@users.noreply.github.com>
2023-01-18 09:25:34 +0700

    Add files via upload

---
+++ php/kalkulator.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+	<title>KALKULATOR</title>
+</head>
+<body>
+	<?php 
+	if(isset($_POST['hitung'])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bil1+$bil2;
+			break;
+			case 'kurang':
+				$hasil = $bil1-$bil2;
+			break;
+			case 'kali':
+				$hasil = $bil1*$bil2;
+			break;
+			case 'bagi':
+				$hasil = $bil1/$bil2;
+			break;			
+		}
+	}
+	?>
+	<div class="kalkulator">
+		<h2 class="judul">KALKULATOR</h2>
+        <hr>
+		<form method="post" action="kalkulator.php">			
+        </p><input type="number" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan1"><p>
+			<input type="number" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan2"><br><br>
+			<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
+			</select>
+			<input type="submit" name="hitung" value="Hitung" class="tombol">											
+		</form>
+		<?php if(isset($_POST['hitung'])){ ?>
+			<input type="text" value="<?php echo $hasil; ?>" class="bil" readonly>
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil" readonly>
+		<?php } ?>			
+	</div>
+</body>
+</html>
